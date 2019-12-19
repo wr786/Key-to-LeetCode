69497d175803f8f1c3874d4dd8c4773611197350
wr786 <wr786@users.noreply.github.com>
2019-12-20 01:26:35 +0800

    2019/12/20

--- 343 Integer Break.cpp
+++ 343 Integer Break.cpp
@@ -6,7 +6,7 @@ public:
         dp[2] = 1;
         for(int i=3; i<=n; i++) {
             for(int j=0; j<i; j++) {
-                int mul1 = max(j, dp[j]); // 不一定要拆i，留着i可能更好
+                int mul1 = max(j, dp[j]); // maybe just use j itself is better.
                 int mul2 = max(i-j, dp[i-j]);
                 dp[i] = max(dp[i], mul1 * mul2);
             }
